35781eea510b268ac6496dab1ca67eb46d3d8e5c
vagrant <vagrant@vagrant.vm>
2020-05-19 09:45:09 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+const process = require('process');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec)
+
+async function executeDocerCompose(serviceName, pushName) {
+  try {
+    const { buildStdout, buildStderr } = await exec(`docker-compose -f ./docker-compose.yml build ${serviceName}`);
+    console.log('stdout:', stdout);
+    console.log('stderr:', stderr);
+  } catch (e) {
+    console.error(e); // should contain code (exit code) and signal (that caused the termination).
+  }
+}
+
+try {
+  // `service-name` input defined in action metadata file
+  const serviceName = core.getInput('service-name') || "noting";
+  console.log(`task param is ${serviceName}!`);
+
+
+  // `push-name` input defined in action metadata file
+  const pushName = core.getInput('push-name') || "noting";
+  console.log(`task param is ${pushName}!`);
+  
+  // `working-directory` input defined in action metadata file
+  const workingDirectory = core.getInput('working-directory')  || "nothing";
+  console.log(`working-directory param is ${workingDirectory}!`);
+ 
+  fs.statSync(workingDirectory);
+  process.chdir(workingDirectory);
+  
+  executeDocerCompose(serviceName, pushName);
+} catch (error) {
+  core.setFailed(error.message);
+}
